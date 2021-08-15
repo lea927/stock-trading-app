018159b99c27db85bea238a0e165e9cee2330cbb
lea927 <lxthym@icloud.com>
2021-08-15 21:24:16 +0800

    Adds Role model to specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,15 @@
 FactoryBot.define do
   factory :user do
     email { 'test@example.com' }
-    password { 'f4k3p455w0rd' }
+    password { 'test123' }
+    password_confirmation { 'test123' }
+    role_id { 1 }
     # using dynamic attributes over static attributes in FactoryBot
 
     # if needed
     # is_active true
+  end
+  factory :role do 
+    name { 'Admin' }
   end
 end
--- spec/system/login_users_spec.rb
+++ spec/system/login_users_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'logging in users', type: :system do
+  let(:role) do
+    @role = FactoryBot.create(:role)
+  end
   let(:user) do
     @user = FactoryBot.create(:user)
   end
 
+  
   before do
     driven_by(:rack_test)
+    role
     user
     visit new_user_session_path
   end
