201664b6743d31dd45e87623c9de339b6089b5b5
lea927 <lxthym@icloud.com>
2021-08-15 15:51:17 +0800

    Adds example to validate sign in path visited

---
+++ spec/system/login_users_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "logging in users", type: :system do
+  let(:user) do
+    @user = FactoryBot.create(:user)
+  end
+
+  before do
+    driven_by(:rack_test)
+    user
+    visit new_user_session_path
+  end
+
+  it 'validates visited path' do
+    expect(page).to have_current_path new_user_session_path
+  end
+end
