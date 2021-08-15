3d1eaf33dcab1254621fa524a586a1940e58c100
yvettetan <yvettetan97@gmail.com>
2021-08-15 14:34:11 +0800

    Updates database

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_08_14_132310) do
   enable_extension "plpgsql"
 
   create_table "roles", force: :cascade do |t|
-    t.string "role_name"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
