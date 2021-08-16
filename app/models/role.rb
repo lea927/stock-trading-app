42bac053048acfb7e74dbf242ab7e323a54c5b96
yvettetan <yvettetan97@gmail.com>
2021-08-16 20:46:41 +0800

    Updates role and user models

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,4 @@
 class Role < ApplicationRecord
-  has_many :users, dependent: :restrict_with_exception
+  has_many :users, dependent: :destroy
+  accepts_nested_attributes_for :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :role
+  belongs_to :role, optional: true
+
+  validates :first_name, :last_name, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
