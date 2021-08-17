d78eca836afd8824222ee6f91b883609c5107fc7
yvettetan <yvettetan97@gmail.com>
2021-08-17 15:48:47 +0800

    Generates and configures devise registrations controller for user signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  # allow role_id to be accepted as part of signup parameters
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[role_id first_name last_name email password])
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password) }
-  end
 end
