4306fd94451aa1d541405b0f70085d2f10cb155f
lea927 <lxthym@icloud.com>
2021-08-17 20:29:24 +0800

    Updates login_tdd

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,17 +1,14 @@
 FactoryBot.define do
+  factory :role do
+    name { 'Admin' }
+  end
+
   factory :user do
     first_name { 'John' }
     last_name { 'Doe' }
     email { 'test@example.com' }
     password { 'test123' }
     password_confirmation { 'test123' }
-    # using dynamic attributes over static attributes in FactoryBot
-
-    # if needed
-    # is_active true
-
-    factory :admin do
-      role { 1 }
-    end
+    role_id { 1 }
   end
 end
--- spec/system/login_users_spec.rb
+++ spec/system/login_users_spec.rb
@@ -1,22 +1,28 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'logging in users', type: :system do
-#   let(:admin_role) do
-#     Role.find_or_create_by({ name: 'Admin' })
-#   end
-#   let(:admin_user) do
-#     User.create!(
-#       first_name: 'Bob', last_name: 'Marley', email: 'test@example.com', password: 'test123', role: 1
-#     )
-#   end
+RSpec.describe 'logging in users', type: :system, driver: :selenium_chrome, js: true do
+  let(:role) do
+    @role = FactoryBot.create(:role)
+  end
+  
+  let(:user) do
+    @user = FactoryBot.create(:user)
+  end
 
-#   it 'successfully signs in' do
-#     visit new_user_session_path
-#     login_as(admin_user)
-#     expect(page).to have_content('Signed in successfully.')
-#   end
+  it 'validates sign in path' do
+    visit new_user_session_path
+    expect(page).to have_current_path new_user_session_path
+    sleep(2)
+  end
 
-#   it 'checks if user is signed up with proper role' do
-#     expect(admin_user.role.name).to eq('Admin')
-#   end
-# end
+  it 'successfully signs in' do
+    role && user
+    visit new_user_session_path
+    within 'form' do
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      click_on 'Log in'
+    end
+    sleep(2)
+  end
+end
