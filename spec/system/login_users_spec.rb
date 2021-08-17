5d7f71c33d3ed7df8e3f833cbce50125404367b1
yvettetan <yvettetan97@gmail.com>
2021-08-17 17:43:23 +0800

    Undo login/sign up specs

--- spec/system/login_users_spec.rb
+++ spec/system/login_users_spec.rb
@@ -1,22 +1,22 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'logging in users', type: :system do
-  let(:admin_role) do
-    Role.find_or_create_by({ name: 'Admin' })
-  end
-  let(:admin_user) do
-    User.create!(
-      first_name: 'Bob', last_name: 'Marley', email: 'test@example.com', password: 'test123', role: 1
-    )
-  end
+# RSpec.describe 'logging in users', type: :system do
+#   let(:admin_role) do
+#     Role.find_or_create_by({ name: 'Admin' })
+#   end
+#   let(:admin_user) do
+#     User.create!(
+#       first_name: 'Bob', last_name: 'Marley', email: 'test@example.com', password: 'test123', role: 1
+#     )
+#   end
 
-  it 'successfully signs in' do
-    visit new_user_session_path
-    login_as(admin_user)
-    expect(page).to have_content('Signed in successfully.')
-  end
+#   it 'successfully signs in' do
+#     visit new_user_session_path
+#     login_as(admin_user)
+#     expect(page).to have_content('Signed in successfully.')
+#   end
 
-  it 'checks if user is signed up with proper role' do
-    expect(admin_user.role.name).to eq('Admin')
-  end
-end
+#   it 'checks if user is signed up with proper role' do
+#     expect(admin_user.role.name).to eq('Admin')
+#   end
+# end
