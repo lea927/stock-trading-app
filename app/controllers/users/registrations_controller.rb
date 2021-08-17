65b860b38c378ae4ae62e7112c7f35e65826cb0e
yvettetan <yvettetan97@gmail.com>
2021-08-17 17:28:02 +0800

    Updates devise registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params # , only: [:create]
+  before_action :configure_account_update_params # , only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -42,12 +42,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:role_id, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[role_id first_name last_name])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
   end
 
   # The path used after sign up.
