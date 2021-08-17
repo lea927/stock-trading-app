4fd4afa0b781d2de8be4383ba22088ad16901f86
lea927 <lxthym@icloud.com>
2021-08-17 23:15:18 +0800

    Adds happy specs for login and signup

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,14 +1,10 @@
 FactoryBot.define do
-  factory :role do
-    name { 'Admin' }
-  end
-
   factory :user do
     first_name { 'John' }
     last_name { 'Doe' }
     email { 'test@example.com' }
     password { 'test123' }
     password_confirmation { 'test123' }
-    role_id { 1 }
+    role_id { [Role.find_or_create_by(name: 'Admin'), Role.find_or_create_by(name: 'Broker'), Role.find_or_create_by(name: 'Buyer')] }
   end
 end
--- spec/system/login_users_spec.rb
+++ spec/system/login_users_spec.rb
@@ -4,16 +4,13 @@ RSpec.describe 'logging in users', type: :system, driver: :selenium_chrome, js: 
   let(:reset_id) do
     Role.connection.execute('ALTER SEQUENCE roles_id_seq RESTART')
   end
-  let(:role) do
-    @role = FactoryBot.create(:role)
-  end
-
   let(:user) do
     @user = FactoryBot.create(:user)
   end
 
   before do
     reset_id
+    user
   end
 
   it 'validates sign in path' do
@@ -23,7 +20,6 @@ RSpec.describe 'logging in users', type: :system, driver: :selenium_chrome, js: 
   end
 
   it 'successfully signs in' do
-    role && user
     visit new_user_session_path
     fill_in 'user_email', with: user.email
     fill_in 'user_password', with: user.password
