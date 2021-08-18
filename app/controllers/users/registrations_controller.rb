f22f331e5f80dd1e6b1bab88a41a93fa641bfa62
yvettetan <yvettetan97@gmail.com>
2021-08-18 20:24:17 +0800

    Adds permits

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,12 +42,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[role_id first_name last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[role_id first_name last_name email password password_confirmation])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name email password password_confirmation])
   end
 
   # The path used after sign up.
