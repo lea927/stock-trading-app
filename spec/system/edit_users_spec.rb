2d03fa11c8c842e6e2308c7356c17334a8e4b5f8
lea927 <lxthym@icloud.com>
2021-08-20 01:18:32 +0800

    Adds sad tests on edit users specs

--- spec/system/edit_users_spec.rb
+++ spec/system/edit_users_spec.rb
@@ -26,6 +26,38 @@ RSpec.describe 'editing users', type: :system, driver: :selenium_chrome, js: tru
     fill_in 'user_password', with: 'janedoe123edited'
     fill_in 'user_password_confirmation', with: 'janedoe123edited'
   end
+  let(:blank_firstname) do
+    page.select 'Broker', :from => 'user[role_id]'
+    fill_in 'user_first_name', with: ''
+    fill_in 'user_last_name', with: 'Doe Edited'
+    fill_in 'user_email', with: 'janedoe_broker_edited@email.com'
+    fill_in 'user_password', with: 'janedoe123edited'
+    fill_in 'user_password_confirmation', with: 'janedoe123edited'
+  end
+  let(:blank_lastname) do
+    page.select 'Broker', :from => 'user[role_id]'
+    fill_in 'user_first_name', with: 'Jane Edited'
+    fill_in 'user_last_name', with: ''
+    fill_in 'user_email', with: 'janedoe_broker_edited@email.com'
+    fill_in 'user_password', with: 'janedoe123edited'
+    fill_in 'user_password_confirmation', with: 'janedoe123edited'
+  end
+  let(:blank_pw) do
+    page.select 'Broker', :from => 'user[role_id]'
+    fill_in 'user_first_name', with: 'Jane Edited'
+    fill_in 'user_last_name', with: 'Doe Edited'
+    fill_in 'user_email', with: 'janedoe_broker_edited@email.com'
+    fill_in 'user_password', with: ''
+    fill_in 'user_password_confirmation', with: ''
+  end
+  let(:blank_email) do
+    page.select 'Broker', :from => 'user[role_id]'
+    fill_in 'user_first_name', with: 'Jane Edited'
+    fill_in 'user_last_name', with: 'Doe Edited'
+    fill_in 'user_email', with: ''
+    fill_in 'user_password', with: 'janedoe123edited'
+    fill_in 'user_password_confirmation', with: 'janedoe123edited'
+  end
   before do
     reset_id
     role
@@ -45,6 +77,32 @@ RSpec.describe 'editing users', type: :system, driver: :selenium_chrome, js: tru
       fillout_form
       click_on 'Update User'
       sleep(2)
+    end
+  end
+  describe 'error messages' do
+    it 'fills out without first name' do
+      visit edit_users_admin_path(id: broker.id)
+      blank_firstname
+      click_on 'Update User'
+      sleep(2)
+    end
+    it 'fills out without last name' do
+      visit edit_users_admin_path(id: broker.id)
+      blank_lastname
+      click_on 'Update User'
+      sleep(2)
+    end
+    it 'fills out without email' do
+      visit edit_users_admin_path(id: broker.id)
+      blank_email
+      click_on 'Update User'
+      sleep(2)
+    end
+    it 'fills out without password' do
+      visit edit_users_admin_path(id: broker.id)
+      blank_pw
+      click_on 'Update User'
+      expect(page).to have_content "Password can't be blank"
     end
   end
 end
