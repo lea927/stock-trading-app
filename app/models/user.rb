e6fd3d368525d855d565d070d8beec0c8d161ebf
yvettetan <yvettetan97@gmail.com>
2021-08-19 21:20:46 +0800

    Creates user model spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :role, optional: true
+  belongs_to :role
 
   validates :first_name, :last_name, :email, :password, presence: true
 
---
+++ spec/model/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :first_name }
+    it { is_expected.to validate_presence_of :last_name }
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :password }
+  end
+
+  describe 'relationships' do
+    it { is_expected.to belong_to :role }
+  end
+end
