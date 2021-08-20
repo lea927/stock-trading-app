110f859ec9930cf49be6ada19fb87c2e3d1f5701
lea927 <lxthym@icloud.com>
2021-08-21 02:10:42 +0800

    Fixes some rubocop offenses on users request spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -15,34 +15,37 @@ RSpec.describe 'users controller', type: :request do
   let(:create_user) do
     User.create!(email: 'janedoe_broker@email.com', password: 'janedoe123', role_id: 2, first_name: 'Jane', last_name: 'Doe')
   end
-  let(:prepare_database) do
-    create_roles
-    create_admin
-    create_user
+  let(:login_user) do
+    login_as(@user, scope: :user)
   end
 
   before do
     reset_id
-    prepare_database
-  end
-
-  before do
-    login_as(@user, scope: :user)
+    create_roles && create_admin && create_user
+    login_user
   end
 
   describe 'index' do
     it 'requests all list of users' do
       get users_admin_index_path
-      expect(response).to be_successful
       expect(response.body).to include('janedoe_broker@email.com')
+    end
+
+    it 'checks response' do
+      get users_admin_index_path
+      expect(response).to be_successful
     end
   end
 
   describe 'GET #index' do
     it 'renders the index template' do
       get users_admin_index_path
-      expect(response).to render_template('layouts/_header')
       expect(response).to render_template('users/index')
+    end
+
+    it 'renders header layout' do
+      get users_admin_index_path
+      expect(response).to render_template('layouts/_header')
     end
 
     it 'renders the application layout' do
