fd36dc0ca368bc0d97ba9a02a00da841a7480a7c
yvettetan <yvettetan97@gmail.com>
2021-08-24 19:49:23 +0800

    Configures environment for gmail and sets env variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,24 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :sendmail
+# Defaults to:
+# config.action_mailer.sendmail_settings = {
+#   location: '/usr/sbin/sendmail',
+#   arguments: '-i'
+# }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'tom.stockit@gmail.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'example.com', #change to domain name
+  user_name:            ENV["MAILER_EMAIL"],
+  password:             ENV["MAILER_PASSWORD"],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 end
