9014c5c15da947fa82ff2265f0923f28b721f426
yvettetan <yvettetan97@gmail.com>
2021-08-25 22:49:23 +0800

    Removes duplicate code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'tom.stockit@gmail.com'}
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
@@ -79,7 +79,7 @@ Rails.application.configure do
   user_name:            ENV["MAILER_EMAIL"],
   password:             ENV["MAILER_PASSWORD"],
   authentication:       'plain',
-  enable_starttls_auto: true,
+  # enable_starttls_auto: true,
   openssl_verify_mode: "none",
  }
 end
