1f122d04aa12c067a306afd44323f00af0f82573
yvettetan <yvettetan97@gmail.com>
2021-08-25 23:39:04 +0800

    Fixes smtp_settings configuration for gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,16 +62,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # config.action_mailer.delivery_method = :sendmail
-# Defaults to:
-# config.action_mailer.sendmail_settings = {
-#   location: '/usr/sbin/sendmail',
-#   arguments: '-i'
-# }
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'tom.stockit@gmail.com'}
 
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
@@ -79,7 +75,6 @@ Rails.application.configure do
   user_name:            ENV["MAILER_EMAIL"],
   password:             ENV["MAILER_PASSWORD"],
   authentication:       'plain',
-  # enable_starttls_auto: true,
-  openssl_verify_mode: "none",
+  enable_starttls_auto: true,
  }
 end
