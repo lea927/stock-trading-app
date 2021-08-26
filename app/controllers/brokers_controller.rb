e6c9e57c3313d9dc7bc1f398d7f801ff1bbc4ad5
lea927 <lxthym@icloud.com>
2021-08-27 00:00:47 +0800

    Adds action to approve broker

--- app/controllers/brokers_controller.rb
+++ app/controllers/brokers_controller.rb
@@ -5,4 +5,18 @@ class BrokersController < ApplicationController
     # if params[:approved] == "false"
     @unapproved_users = User.where(approved: false)
   end
+
+  def edit; end
+
+  def update 
+    @user = User.find(params[:id])
+    @user.update(:approved=>true)
+    redirect_to brokers_path, notice: "Broker was successfully approved."
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:approved) 
+  end
 end
