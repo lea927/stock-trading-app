84b32b28bba88d2830251483fa451b1c9980b14e
lea927 <lxthym@icloud.com>
2021-08-28 20:12:36 +0800

    Fixes error on users edit path

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params # , only: [:create]
   before_action :configure_account_update_params # , only: [:update]
+  before_action :unapproved_users
 
   # GET /resource/sign_up
   # def new
@@ -61,6 +62,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def unapproved_users 
+    @unapproved_users = User.where(approved: false)
+  end
+
   private
 
   def user_params
