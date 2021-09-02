c72ef23ebcc7503782f84315238566339a943aa2
yvettetan <yvettetan97@gmail.com>
2021-09-02 16:36:21 +0800

    Creates method to check if stock exists in database

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,7 +2,7 @@ class Stock < ApplicationRecord
   has_many :transactions
   has_many :users, through: :transactions
   validates :symbol, { presence: true, uniqueness: true }
-  
+
   #self: don't need an instance of stock class. can directly call this method from the class
   def self.iex_api
     IEX::Api::Client.new(
@@ -20,5 +20,9 @@ class Stock < ApplicationRecord
       #return nil in order to display a message through the controller when error is caught 
       nil
     end
+  end
+
+  def self.search_db(symbol)
+    where(symbol: symbol).first
   end
 end
