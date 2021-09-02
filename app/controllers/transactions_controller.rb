1f66df25f1633dc250a30422c44d5e9c814bcee4
yvettetan <yvettetan97@gmail.com>
2021-09-02 16:38:34 +0800

    Adds create action to transactions controller to add stocks to user portfolio

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,4 +4,13 @@ class TransactionsController < ApplicationController
     def my_portfolio
       @stocks = current_user.stocks
     end
+
+    def create 
+      #only create a new lookup of stock if it doesn't exist in the current database
+      stock = Stock.search_db(params[:symbol]) || Stock.new_lookup(params[:symbol])
+      stock.save
+      @transaction = Transaction.create(user: current_user, stock: stock)
+      flash[:notice] = "#{stock.company_name} was successfully added to your portfolio"
+      redirect_to my_portfolio_path
+    end
 end
