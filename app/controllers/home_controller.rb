5ce0cc8e6e8a1c154c324e9085f8a09af490a401
yvettetan <yvettetan97@gmail.com>
2021-09-02 17:42:31 +0800

    Fixes home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   def index
     if  current_user&.admin?
         redirect_to users_admin_index_path
-    else
+    elsif current_user&.broker? || current_user&.buyer?
         redirect_to my_portfolio_path
     end
   end
