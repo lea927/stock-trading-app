8cbaeefe0dbf0104d3df2bd6aabc82d60dc75a8c
lea927 <lxthym@icloud.com>
2021-09-02 23:10:04 +0800

    Adds stock market path and action for buyer

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     resources :pending_brokers, only: [:index, :update]
   end
   get 'my_portfolio', to:'transactions#my_portfolio'
+  get 'buyer_stock_market', to:'transactions#buyer_stock_market'
   get 'search_stock', to: 'stocks#search'
   get 'stocks/search'
   resources :transactions, only: [:create]
