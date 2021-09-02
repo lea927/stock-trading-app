38edef95c92a65e68fa8a7b852c8c553520fbda3
yvettetan <yvettetan97@gmail.com>
2021-09-02 10:58:10 +0800

    Installs fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/scss/bootstrap'
 require("../stylesheets/application.scss")
+import "@fortawesome/fontawesome-free/css/all"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
