8c3e4ce8090366b6aaacd8ce9cf86bca9eb78cc2
lea927 <lxthym@icloud.com>
2021-09-03 22:32:59 +0800

    Creates a separate action for creating a transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -7,14 +7,18 @@ class TransactionsController < ApplicationController
 
     def create 
       #only create a new lookup of stock if it doesn't exist in the current database
-      stock = Stock.search_db(params[:symbol]) || Stock.new_lookup(params[:symbol])
-      stock.save
-      @transaction = Transaction.create(user: current_user, stock: stock)
-      flash[:notice] = "#{stock.company_name} was successfully added to your portfolio"
-      redirect_to my_portfolio_path
+      @stock = Stock.search_db(params[:symbol]) || Stock.new_lookup(params[:symbol])
+      @stock.save
+      save_transaction
     end
 
     def buyer_stock_market
       @stocks = User.find_by(role_id: 2, approved: true).stocks
+    end
+
+    def save_transaction
+      @transaction = Transaction.create!(user: current_user, stock: @stock)
+      flash[:notice] = "#{@stock.company_name} was successfully added to your portfolio"
+      redirect_to my_portfolio_path
     end
 end
