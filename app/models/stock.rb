5dd1b2165a293167209b1df68d63a925c4781626
yvettetan <yvettetan97@gmail.com>
2021-09-23 17:13:38 +0800

    Adds dependent option

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,5 @@
 class Stock < ApplicationRecord
-  has_many :transactions
+  has_many :transactions, dependent: :destroy
   has_many :users, through: :transactions
   validates :symbol, { presence: true, uniqueness: true }
 
